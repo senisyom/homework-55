3d46945c3e0cb5a95f2314598b0780d8e40fd3e4
Begmatov Almaz <lolkek13@inbox.ru>
2024-09-26 15:52:34 +0600

    Создал гит и так же добавил верстку бургера вместе со стилями

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+
+
+const App = () => {
+
+  return (
+    <div className="Burger">
+      <div className="BreadTop">
+        <div className="Seeds1"></div>
+        <div className="Seeds2"></div>
+      </div>
+      <div className="Salad"></div>
+      <div className="Cheese"></div>
+      <div className="Meat"></div>
+      <div className="BreadBottom"></div>
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/Burger/Burger.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const Burger = () => {
+    return (
+      <div className="Burger">
+        <div className="BreadTop">
+          <div className="Seeds1"></div>
+          <div className="Seeds2"></div>
+        </div>
+        <div className="Salad"></div>
+        <div className="Cheese"></div>
+        <div className="Meat"></div>
+        <div className="BreadBottom"></div>
+      </div>
+    );
+}
+
+export default Burger
